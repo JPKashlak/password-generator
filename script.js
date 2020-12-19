39e2db96eb81ac9e719c410a4b905a2152038c25
jpkashlak <jpkashlak@gmail.com>
2020-12-19 12:43:44 -0600

    Password Length Prompt added

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Assignment code here
+var howLong = function() {
+    var passLength = prompt("How many characters would you like your new password to be?\n(Please choose from 8 to 128)");
+        if (passLength < 8 || passLength > 128) {
+        alert("Please choose a number from 8 to 128");
+        howLong();
+    }
+    else {
+        alert("Okay! I'll make sure your password is " + passLength + " characters long!")
+    }
+}
+var lowerOption = function() {}
+var upperOption = function() {}
+var numericOption = function() {}
+var specialOption = function() {}
+
+
+
 var generatePassword = function() {
     alert("Welcome to Password Generator!");
-    confirm("Do you want a fancy password?");
+    howLong();
+
 }    
 
 // Get references to the #generate element
